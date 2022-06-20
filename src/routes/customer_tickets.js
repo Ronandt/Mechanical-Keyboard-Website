c399f6621f3ae8bebe28436a57249b53179772d3
andy <luthandyka.business@gmail.com>
2022-06-20 19:56:30 +0800

    Added ticket status view
    For users to see the status of their ticket-status. It looks very plain
    as there is no figma mockup and I'm too tired and lazy to do it today.

--- src/routes/customer_tickets.js
+++ src/routes/customer_tickets.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const ticketRouter = express.Router();
 const Ticket = require("../models/Ticket");
+const User = require("../models/User");
 
 ticketRouter.get("/", async (req, res) => {
     if (req.isUnauthenticated()) {
@@ -27,6 +28,20 @@ ticketRouter.post("/", async (req, res) => {
 
     req.flash("success", "Ticket submitted!");
     return res.redirect("/ticket");
+});
+
+ticketRouter.get("/status", async (req, res) => {
+    if (req.isUnauthenticated()) {
+        req.flash("error", "To view tickets, you must be logged in.");
+        return res.redirect("/login");
+    }
+
+    const userID = req.user.id;
+    const tickets = await Ticket.findAll({ where: { authorID: userID } });
+
+    return res.render("./customers/ticket/ticket-status", {
+        tickets,
+    });
 });
 
 module.exports = ticketRouter;
